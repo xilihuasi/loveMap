8ba062c6a9ef6558c74b662e7963246fa5011f57
xilihuasi <2857818553@qq.com>
2017-06-13 00:13:41 +0800

    添加百度天气信息

--- pages/heart/heart.js
+++ pages/heart/heart.js
@@ -1,12 +1,37 @@
 //heart.js
+// 引用百度地图微信小程序JSAPI模块 
+var bmap = require('../../libs/bmap-wx.js'); 
 //获取应用实例
-var app = getApp()
+var app = getApp();
 Page({
   data: {
+  	weatherData: null
   },
   onLoad() {
+  	var that = this; 
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({ 
+        ak: 'I6opjpKWyAp9oVARZfSuLy28KOAtGpWx' 
+    }); 
+    var fail = function(data) { 
+        console.log(data) 
+    }; 
+    var success = function(data) { 
+    	console.log(data);
+        var weatherData = data.currentWeather[0]; 
+        weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' +'日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' +'天气：' + weatherData.weatherDesc + '\n' +'风力：' + weatherData.wind + '\n'; 
+        that.setData({ 
+            weatherData: weatherData 
+        }); 
+    } 
+    // 发起weather请求 
+    BMap.weather({ 
+        fail: fail, 
+        success: success 
+    }); 
   },
   bindtap() {
     console.log('tap');
   },
+
 })
