cfdb1efbcade7af2cb6cbe8512c110c3f3436ce6
xilihuasi <2857818553@qq.com>
2017-06-14 23:56:51 +0800

    添加格式化pm25、计算日期间隔工具函数

--- utils/util.js
+++ utils/util.js
@@ -16,6 +16,42 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+function formatPm25(num) {
+  num -=0;
+  let txt = '';
+  switch (true) {
+    case num <= 50:
+      txt = '优';
+      break;
+    case num <= 100:
+      txt = '良';
+      break;
+    case num <= 200:
+      txt = '轻度污染';
+      break;
+    case num <= 300:
+      txt = '中度污染';
+      break;
+    case num > 300:
+      txt = '重度污染';
+      break;
+    default:
+      break;
+  }
+  return txt;
+}
+
+function getNumBetweenDays(start, end) {
+  const startTime = new Date(start).getTime() || null;
+  const endTime = new Date(end).getTime() || new Date().getTime();
+  let num = 0;
+  if (startTime) {
+    num = Math.floor(Math.abs(startTime - endTime) / (1000 * 60 * 60 * 24));
+  }
+  return num;
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatPm25: formatPm25,
+  getNumBetweenDays: getNumBetweenDays
 }
